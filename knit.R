7e4cfd71d133c42fed931a12d45cef22c38aa3ce
Charlie9898 <che22@ncsu.edu>
2022-10-07 23:04:38 -0400

    Finish API functions and start EDA.

---
+++ knit.R
@@ -0,0 +1,5 @@
+# Chuanni He
+# This is to knit the project Rmd file.
+rmarkdown::render("C:/Users/hechu/OneDrive/Courses/ST 558/Project/Project 2/ST-558-Project-2/Project.Rmd",
+                  "github_document",
+                  output_file = "C:/Users/hechu/OneDrive/Courses/ST 558/Project/Project 2/ST-558-Project-2/README.md")
